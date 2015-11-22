1194618ff3c4fc0132eca272a9e0913429909747
Gabor <nemethng@gmail.com>
2015-11-22 19:54:29 +0100

    typo

--- ui.R
+++ ui.R
@@ -21,7 +21,7 @@ shinyUI(
                    h3("About"),
                    h4("This application is designed to calculate the required amount of solvent for a solution."),
                    h4("The user should enter the amount to be solubilised in gramms and the molecular weight of the material."),
-                   h4("After having set the aimed concentration, the user should click on the 'Calculate' button"),
+                   h4("After having set the aimed concentration, the user should click on the 'Calculate' button."),
                    h4("The app returns the input data and the required volume of solvent in mililiters.")
             )
         ),
